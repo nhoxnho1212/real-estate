d5230d277d96ebe64e31446b7727204bdc916fa3
thienntn <nguyentrannhatthien1710@gmail.com>
2021-07-18 12:29:47 +0700

    delete and clean fe source code

--- src/main/webapp/app/shared/model/home-type.model.ts
+++
@@ -1,9 +0,0 @@
-import { IProject } from 'app/shared/model/project.model';
-
-export interface IHomeType {
-  id?: number;
-  typeName?: string | null;
-  projects?: IProject[] | null;
-}
-
-export const defaultValue: Readonly<IHomeType> = {};
