46650f058acc2a19c3a79916ce34f0e32969463e
tung <nhoxnho1212@gmail.com>
2021-08-18 21:23:33 +0700

    Fix update project when missing user

--- src/main/java/com/ou/ret/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/ou/ret/service/impl/ProjectServiceImpl.java
@@ -1,22 +1,25 @@
 package com.ou.ret.service.impl;
 
 import com.ou.ret.domain.Project;
+import com.ou.ret.domain.User;
 import com.ou.ret.repository.ProjectRepository;
+import com.ou.ret.repository.UserRepository;
 import com.ou.ret.service.ProjectService;
 import com.ou.ret.service.dto.ProjectDTO;
 import com.ou.ret.service.mapper.ProjectMapper;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-
+import com.ou.ret.web.rest.errors.BadRequestAlertException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 /**
  * Service Implementation for managing {@link Project}.
@@ -28,11 +31,13 @@ public class ProjectServiceImpl implements ProjectService {
     private final Logger log = LoggerFactory.getLogger(ProjectServiceImpl.class);
 
     private final ProjectRepository projectRepository;
+    private final UserRepository userRepository;
 
     private final ProjectMapper projectMapper;
 
-    public ProjectServiceImpl(ProjectRepository projectRepository, ProjectMapper projectMapper) {
+    public ProjectServiceImpl(ProjectRepository projectRepository, UserRepository userRepository, ProjectMapper projectMapper) {
         this.projectRepository = projectRepository;
+        this.userRepository = userRepository;
         this.projectMapper = projectMapper;
     }
 
@@ -40,6 +45,14 @@ public class ProjectServiceImpl implements ProjectService {
     public ProjectDTO save(ProjectDTO projectDTO) {
         log.debug("Request to save Project : {}", projectDTO);
         Project project = projectMapper.toEntity(projectDTO);
+        if (Objects.isNull(projectDTO.getUserDTO()) || Objects.isNull(projectDTO.getUserDTO().getId())) {
+            throw new BadRequestAlertException("Invalid user ID", "project", "idinvalid");
+        }
+        Optional<User> user = userRepository.findById(projectDTO.getUserDTO().getId());
+        if (user.isEmpty()) {
+            throw new BadRequestAlertException("Invalid user ID", "project", "idinvalid");
+        }
+        project.setUser(user.get());
         project = projectRepository.save(project);
         return projectMapper.toDto(project);
     }
